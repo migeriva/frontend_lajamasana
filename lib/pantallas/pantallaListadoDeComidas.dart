cdd1a283cc5db846c7d3756ea0fbf4167d5bb462
Miguel Rivadeneira <migeriva@espol.edu.ec>
2021-02-24 01:03:54 -0500

    Pantalla de listado de comidas: Cambio de widgets total, gridview para mostar los listados, problemas con la altura del contenedor de los listados

--- lib/pantallas/pantallaListadoDeComidas.dart
+++ lib/pantallas/pantallaListadoDeComidas.dart
@@ -1,55 +1,193 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:lajamasana/pantallas/pantallaInformacionDeComidas.dart';
+
 class ListadoComidas extends StatefulWidget {
   @override
   _ListadoComidasState createState() => _ListadoComidasState();
 }
 
 class _ListadoComidasState extends State<ListadoComidas> {
-  var _horarios = ["Desayuno", "Almuerzo", "Merienda"];
-  String _hint = "Almuerzo";
+  List<String> _horarios = ["Desayuno", "Almuerzo", "Merienda"];
+
+  String _hint = "Almuerzo"; //Cambiar para que sea de acuerdo a la hora del dia
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body: crearContenido(),
+    );
+  }
+
+  Widget crearContenido() {
+    return ListView(
+      children: [
+        contenedorCabecera(),
+        contenedorDropdown(),
+        menu(),
+        listadoComidas(),
+      ],
+    );
+  }
+
+  Widget contenedorCabecera() {
+    return Row(
+      children: [
+        Container(
+          alignment: Alignment.topLeft,
+          padding: EdgeInsets.only(left: 10, top: 10),
+          width: MediaQuery.of(context).size.width * 0.5,
+          height: 80,
+          child: RichText(
+            text: TextSpan(
+                text: "Perfil\n",
+                style: TextStyle(
+                  fontFamily: "Roboto",
+                  color: Color(0xff969FAA),
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+                children: [
+                  TextSpan(
+                    text: "Perder Peso",
+                    style: TextStyle(
+                      fontFamily: "Roboto",
+                      color: Colors.black,
+                      fontSize: 21,
+                      fontStyle: FontStyle.italic,
+                    ),
+                  ),
+                ]),
+          ),
+        ),
+        Container(
+          alignment: Alignment.centerRight,
+          padding: EdgeInsets.only(right: 20),
+          width: MediaQuery.of(context).size.width * 0.5,
+          height: 80,
+          child: Image.asset(
+            "assets/imagenes/logo.png",
+            height: 70,
+            width: 70,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget contenedorDropdown() {
+    return Container(
+      // alignment: Alignment.topLeft,
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      width: MediaQuery.of(context).size.width,
+      height: 70,
+      child: Column(
         children: [
-          contenedorPrincipal(),
-          dropdownHorario(),
-          listado(),
+          Container(
+            alignment: Alignment.topLeft,
+            child: Text(
+              "Categoria",
+              style: TextStyle(
+                fontFamily: "Roboto",
+                color: Color(0xff969FAA),
+                fontSize: 17,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          // Padding(padding: EdgeInsets.only(top: 20) ),
+          DropdownButton(
+            isExpanded: true,
+            items: crearItems(),
+            onChanged: (_valor) => {
+              setState(() {
+                _hint = _valor;
+              })
+            },
+            hint: RichText(
+                //Este widget aun no esta completo, duda de su existencia
+                text: TextSpan(
+              text: "",
+              children: [
+                WidgetSpan(
+                    alignment: PlaceholderAlignment.middle,
+                    child: Image.asset(
+                      "assets/imagenes/sopa.png",
+                      width: 30,
+                      height: 30,
+                    )),
+                TextSpan(
+                  text: _hint,
+                  style: TextStyle(
+                    fontFamily: "Roboto",
+                    fontSize: 18,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            )),
+          ),
         ],
       ),
     );
   }
 
-  Widget listado() {
-    return Container(
-      color: Color(0xff77D353).withOpacity(.06),
-      height: 508,
-      width: MediaQuery.of(context).size.width,
-      child: Stack(
-        children: [
-          Positioned(
-            top: 20,
-            left: 20,
-            child: Text(
-              "Menu",
-              style: TextStyle(
-                fontFamily: "Roboto",
-                fontSize: 20,
-                fontStyle: FontStyle.italic,
-                color: Color(0xff3F9C90),
-                fontWeight: FontWeight.bold,
-              ),
-            ),
+  List<DropdownMenuItem> crearItems() {
+    List<DropdownMenuItem> items = [];
+
+    for (int i = 0; i < _horarios.length; i++) {
+      items.add(DropdownMenuItem(
+        value: _horarios[i],
+        child: Text(
+          _horarios[i],
+          style: TextStyle(
+            fontFamily: "Roboto",
+            fontSize: 18,
+            color: Colors.black,
           ),
-          Positioned(
-            top: 70,
-            left: 30,
-            child: Container(
-              width: 160,
-              height: 250,
+        ),
+      ));
+    }
+
+    return items;
+  }
+
+  Widget menu() {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      padding: EdgeInsets.only(left: 20, bottom: 20, top: 10),
+      color: Color(0xff77D353).withOpacity(.06),
+      alignment: Alignment.centerLeft,
+      child: Text(
+        "Menú",
+        style: TextStyle(
+          fontFamily: "Roboto",
+          fontSize: 20,
+          fontStyle: FontStyle.italic,
+          color: Color(0xff3F9C90),
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
+  Widget listadoComidas() {
+    return Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        // padding: EdgeInsets.only(bottom: 60),
+        color: Color(0xff77D353).withOpacity(.06),
+        child: GridView.count(
+          controller: new ScrollController(keepScrollOffset: true), //Scroll
+          scrollDirection: Axis.vertical,
+          childAspectRatio: (160 /
+              250), // Tamanio de la presentaqcion del gridview (width/height)
+          crossAxisCount: 2,
+          mainAxisSpacing: 20,
+          crossAxisSpacing: 30,
+          padding: EdgeInsets.only(top: 30, left: 20, right: 20),
+          children: [
+            Container(
               decoration: BoxDecoration(
                 border: Border.all(width: 3, color: Color(0xff77D353)),
                 borderRadius: BorderRadius.circular(30),
@@ -57,29 +195,25 @@ class _ListadoComidasState extends State<ListadoComidas> {
               child: Column(
                 children: [
                   Image.asset("assets/imagenes/calysupremo.png"),
-                  Text("CalySupremo"),
+                  Text("CalySupremo",
+                      style: TextStyle(fontFamily: "Roboto", fontSize: 17)),
                   Padding(
                     padding: EdgeInsets.only(top: 10),
-                    child: RaisedButton(
-                        color: Color(0xff4FD053),
-                        onPressed: () {
-                          Navigator.of(context).push(MaterialPageRoute(
-                              builder: (context) => PantallaDeComida()));
-                        },
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(50)),
-                        child: Text("Ver mas")),
-                  )
+                  ),
+                  RaisedButton(
+                    color: Color(0xff4FD053),
+                    onPressed: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => PantallaDeComida()));
+                    },
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50)),
+                    child: Text("Ver mas"),
+                  ),
                 ],
               ),
             ),
-          ),
-          Positioned(
-            top: 70,
-            left: 220,
-            child: Container(
-              width: 160,
-              height: 250,
+            Container(
               decoration: BoxDecoration(
                 border: Border.all(width: 3, color: Color(0xff77D353)),
                 borderRadius: BorderRadius.circular(30),
@@ -87,111 +221,25 @@ class _ListadoComidasState extends State<ListadoComidas> {
               child: Column(
                 children: [
                   Image.asset("assets/imagenes/rollycaly.png"),
-                  Text("RollyCaly"),
+                  Text("Rollycaly",
+                      style: TextStyle(fontFamily: "Roboto", fontSize: 17)),
                   Padding(
                     padding: EdgeInsets.only(top: 10),
-                    child: RaisedButton(
-                      color: Color(0xff4FD053),
-                      onPressed: () {
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => PantallaDeComida()));
-                      },
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(50)),
-                      child: Text("Ver mas"),
-                    ),
-                  )
+                  ),
+                  RaisedButton(
+                    color: Color(0xff4FD053),
+                    onPressed: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => PantallaDeComida()));
+                    },
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50)),
+                    child: Text("Ver mas"),
+                  ),
                 ],
               ),
             ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget contenedorPrincipal() {
-    return Padding(
-      padding: EdgeInsets.only(top: 25),
-      child: Container(
-        color: Colors.white,
-        height: 90,
-        width: MediaQuery.of(context).size.width,
-        child: Row(
-          children: [
-            Padding(
-                padding: EdgeInsets.only(left: 20, right: 30),
-                child: Container(
-                  child: Column(
-                    children: [
-                      Padding(padding: EdgeInsets.only(top: 10)),
-                      Text(
-                        "Perfil",
-                        style: TextStyle(
-                          color: Color(0xff969FAA),
-                          fontFamily: "Roboto",
-                          fontSize: 17,
-                        ),
-                      ),
-                      Padding(padding: EdgeInsets.only(top: 15)),
-                      Text(
-                        "Perder Peso",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontFamily: "Roboto",
-                          fontSize: 20,
-                        ),
-                      ),
-                    ],
-                  ),
-                )),
-            Padding(
-              padding: EdgeInsets.only(left: 155, right: 10),
-              child: Image.asset(
-                "assets/imagenes/logo.png",
-                width: 80,
-              ),
-            )
           ],
-        ),
-      ),
-    );
-  }
-
-  Widget dropdownHorario() {
-    return Container(
-        height: 60,
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 15),
-          child: DropdownButton(
-            isExpanded: true,
-            items: _horarios.map((String item) {
-              return DropdownMenuItem(
-                value: item,
-                child: Text(
-                  item,
-                  style: TextStyle(
-                    fontFamily: "Roboto",
-                    fontSize: 18,
-                    color: Colors.black,
-                  ),
-                ),
-              );
-            }).toList(),
-            onChanged: (_valor) => {
-              setState(() {
-                _hint = _valor;
-              })
-            },
-            hint: Text(
-              _hint,
-              style: TextStyle(
-                fontFamily: "Roboto",
-                fontSize: 18,
-                color: Colors.black,
-              ),
-            ),
-          ),
         ));
   }
 }
