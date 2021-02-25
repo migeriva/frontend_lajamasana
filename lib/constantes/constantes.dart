2fa835f3a21c6e9f4ecc4df725052dab8caa593a
Miguel Rivadeneira <migeriva@espol.edu.ec>
2021-02-25 16:17:51 -0500

    Constantes: clase creada para usar constatnes en todo el proyecto

---
+++ lib/constantes/constantes.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class Constantes {
+
+  //URL
+  //No usadas
+  static final String urlPerfiles = "";
+  static final String urlSuscripciones = "";
+  static final String urlUsuarios = "";
+
+
+  //TextStyle
+
+  static final TextStyle styleLogo = TextStyle(
+    fontSize: 25,
+    fontWeight: FontWeight.bold,
+    fontFamily: "Lato",
+    color: Colors.green,
+  );
+
+  static final TextStyle styleLabel = TextStyle(
+      fontSize: 17,
+      fontFamily: "Lato",
+      fontWeight: FontWeight.bold,
+      color: Colors.black);
+
+  // //TextField
+  // static final TextField textFielUsuario = TextField(
+  //   textAlign: TextAlign.left,
+  //   decoration: InputDecoration(
+  //     enabledBorder: OutlineInputBorder(
+  //         borderSide: BorderSide(color: Color(0xff77D353), width: 3),
+  //         borderRadius: BorderRadius.circular(30)),
+  //     focusedBorder: OutlineInputBorder(
+  //         borderSide: BorderSide(color: Color(0xff77D353), width: 3),
+  //         borderRadius: BorderRadius.circular(30)),
+  //   ),
+  // );
+  // static final TextField textFieldPass = TextField(
+  //   textAlign: TextAlign.left,
+  //   obscureText: true,
+  //   decoration: InputDecoration(
+  //     enabledBorder: OutlineInputBorder(
+  //         borderSide: BorderSide(color: Color(0xff77D353), width: 3),
+  //         borderRadius: BorderRadius.circular(30)),
+  //     focusedBorder: OutlineInputBorder(
+  //         borderSide: BorderSide(color: Color(0xff77D353), width: 3),
+  //         borderRadius: BorderRadius.circular(30)),
+  //   ),
+  // );
+  
+}
