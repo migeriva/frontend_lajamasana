fec4c17b661ad72f58b9780aa24c1bb930c019a9
Miguel Rivadeneira <migeriva@espol.edu.ec>
2021-02-26 18:02:47 -0500

    Widget Notas: Ajuste de la accion del input al terminar la escritura

--- lib/widgets/notas.dart
+++ lib/widgets/notas.dart
@@ -26,115 +26,28 @@ class _NotasComidaState extends State<NotasComida> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-        children: [ 
-          Divider(color: Color(0xff4FD053), height: 30, thickness: 3),
-          //Container completo 
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: 170,
-            child: Column(
-              children: [
-                //Fecha y hora
-                Container(
-                  height: 70,
-                  child: Row(
-                    children: [
-                      //Seleccionar dia
-                      Container(
-                        width: MediaQuery.of(context).size.width * .5,
-                        alignment: Alignment.center,
-                        padding: EdgeInsets.symmetric(horizontal: 35),
-                        child: DropdownButton(
-                          items: crearItems(),
-                          isExpanded: true,
-                          onChanged: (valor) {
-                            setState(() {
-                              _hint = valor;
-                            });
-                          },
-                          hint: Text(
-                            _hint,
-                            style: TextStyle(
-                              fontFamily: "Roboto",
-                              fontSize: 15,
-                              color: Colors.black,
-                            ),
-                          ),
-                          icon: Icon(Icons.calendar_today),
-                        ),
-                      ),
-                      //Seleccionar Hora
-                      Container(
-                        width: MediaQuery.of(context).size.width * .5,
-                        padding: EdgeInsets.symmetric(horizontal: 5),
-                        child: Row(
-                          children: [
-                            Container(
-                              alignment: Alignment.center,
-                              width: MediaQuery.of(context).size.width * 0.3,
-                              child: Text(
-                                _hintHora,
-                                style: TextStyle(
-                                fontFamily: "Roboto",
-                                fontSize: 15,
-                                color: Colors.black,
-                              ),
-                              ),
-                            ),
-                            IconButton(
-                              icon: Icon(Icons.access_time, color: Colors.black,), 
-                              onPressed: () async {
-                                _picked = await seleccionarTiempo();
-                                setState(() {
-                                  _hintHora = _picked.format(context);
-                                });
-                              }
-                            ),
-                          ],
-                        ),
 
-                      ),
-                    ],
-                  ),
-                ),
-                //Notas
-                Container(
-                  height: 100,
-                  alignment: Alignment.center,
-                  padding: EdgeInsets.symmetric(horizontal: 15),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: "Notas...",
-                      hintStyle: TextStyle(
-                        fontFamily: "Roboto",
-                        fontStyle: FontStyle.italic,
-                      ),
-                      prefixIcon: Icon(
-                        Icons.event_note,
-                        color: Colors.black,
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.black, width: 2),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.green, width: 2),
-                      ),
-                    ),
-                    keyboardType: TextInputType.multiline,
-                    minLines: 1,
-                    maxLines: 2,
-                  ),
-                ),
-              ],
-            ),
+    return Column(
+      children: [
+        Divider(color: Color(0xff4FD053), height: 30, thickness: 3),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: 170,
+          child: Column(
+            children: [
+              camposDiaHora(),
+              campoNotas(),
+            ],
           ),
-          Divider(color: Color(0xff4FD053), height: 20, thickness: 3),
-        ],
-      );
+        ),
+        Divider(color: Color(0xff4FD053), height: 20, thickness: 3),
+      ],
+    );
+
   }
 
   List<DropdownMenuItem> crearItems() {
+
     List<DropdownMenuItem> items = [];
 
     for (int i = 0; i < _dias.length; i++) {
@@ -143,15 +56,126 @@ class _NotasComidaState extends State<NotasComida> {
         child: Text(
           _dias[i],
           style: TextStyle(
-            fontFamily: "Roboto",
-            fontSize: 15,
-            color: Colors.black,
-            fontStyle: FontStyle.italic
-          ),
+              fontFamily: "Roboto",
+              fontSize: 15,
+              color: Colors.black,
+              fontStyle: FontStyle.italic),
         ),
       ));
     }
 
     return items;
+
+  }
+
+  Widget camposDiaHora() {
+
+    return Container(
+      height: 70,
+      child: Row(
+        children: [
+          campoDia(),
+          campoHora(),
+        ],
+      ),
+    );
+
+  }
+
+  Widget campoDia() {
+
+    return Container(
+      width: MediaQuery.of(context).size.width * .5,
+      alignment: Alignment.center,
+      padding: EdgeInsets.symmetric(horizontal: 35),
+      child: DropdownButton(
+        items: crearItems(),
+        isExpanded: true,
+        onChanged: (valor) {
+          setState(() {
+            _hint = valor;
+          });
+        },
+        hint: Text(
+          _hint,
+          style: TextStyle(
+            fontFamily: "Roboto",
+            fontSize: 15,
+            color: Colors.black,
+          ),
+        ),
+        icon: Icon(Icons.calendar_today),
+      ),
+    );
+
+  }
+
+  Widget campoHora() {
+
+    return Container(
+      width: MediaQuery.of(context).size.width * .5,
+      padding: EdgeInsets.symmetric(horizontal: 5),
+      child: Row(
+        children: [
+          Container(
+            alignment: Alignment.center,
+            width: MediaQuery.of(context).size.width * 0.3,
+            child: Text(
+              _hintHora,
+              style: TextStyle(
+                fontFamily: "Roboto",
+                fontSize: 15,
+                color: Colors.black,
+              ),
+            ),
+          ),
+          IconButton(
+              icon: Icon(
+                Icons.access_time,
+                color: Colors.black,
+              ),
+              onPressed: () async {
+                _picked = await seleccionarTiempo();
+                setState(() {
+                  _hintHora = _picked.format(context);
+                });
+              }),
+        ],
+      ),
+    );
+
+  }
+
+  Widget campoNotas() {
+
+    return Container(
+      height: 100,
+      alignment: Alignment.center,
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: "Notas...",
+          hintStyle: TextStyle(
+            fontFamily: "Roboto",
+            fontStyle: FontStyle.italic,
+          ),
+          prefixIcon: Icon(
+            Icons.event_note,
+            color: Colors.black,
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.black, width: 2),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.green, width: 2),
+          ),
+        ),
+        keyboardType: TextInputType.multiline,
+        textInputAction: TextInputAction.done,
+        minLines: 1,
+        maxLines: 2,
+      ),
+    );
+    
   }
 }
