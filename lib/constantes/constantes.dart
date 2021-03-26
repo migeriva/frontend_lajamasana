ffeaa7c9049ec765f8568993318d8184a26bd585
Miguel Rivadeneira <migeriva@espol.edu.ec>
2021-03-25 21:19:58 -0500

    Agregadas constantes para la lista de dias y estilos de botones

--- lib/constantes/constantes.dart
+++ lib/constantes/constantes.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 
 class Constantes {
-
   //URL
   //No usadas
   static final String urlPerfiles = "";
   static final String urlSuscripciones = "";
   static final String urlUsuarios = "";
-
 
   //TextStyle
 
@@ -24,29 +22,22 @@ class Constantes {
       fontWeight: FontWeight.bold,
       color: Colors.black);
 
-  // //TextField
-  // static final TextField textFielUsuario = TextField(
-  //   textAlign: TextAlign.left,
-  //   decoration: InputDecoration(
-  //     enabledBorder: OutlineInputBorder(
-  //         borderSide: BorderSide(color: Color(0xff77D353), width: 3),
-  //         borderRadius: BorderRadius.circular(30)),
-  //     focusedBorder: OutlineInputBorder(
-  //         borderSide: BorderSide(color: Color(0xff77D353), width: 3),
-  //         borderRadius: BorderRadius.circular(30)),
-  //   ),
-  // );
-  // static final TextField textFieldPass = TextField(
-  //   textAlign: TextAlign.left,
-  //   obscureText: true,
-  //   decoration: InputDecoration(
-  //     enabledBorder: OutlineInputBorder(
-  //         borderSide: BorderSide(color: Color(0xff77D353), width: 3),
-  //         borderRadius: BorderRadius.circular(30)),
-  //     focusedBorder: OutlineInputBorder(
-  //         borderSide: BorderSide(color: Color(0xff77D353), width: 3),
-  //         borderRadius: BorderRadius.circular(30)),
-  //   ),
-  // );
-  
+  static final styleButton = (color) => ButtonStyle(
+        backgroundColor: MaterialStateProperty.all<Color>(Color(color)),
+        shadowColor: MaterialStateProperty.all<Color>(Colors.transparent),
+        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(30))),
+      );
+
+  //Listas
+
+  static final List<String> dias = [
+    "Lunes",
+    "Martes",
+    "Miercoles",
+    "Jueves",
+    "Viernes",
+    "Sabado",
+    "Domingo"
+  ];
 }
