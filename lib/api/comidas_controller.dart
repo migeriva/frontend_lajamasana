824a46bfb9b7bf5afc9dec2818a56fc618a80b75
Miguel Rivadeneira <migeriva@espol.edu.ec>
2021-04-06 20:32:02 -0500

    Añadido controlador para las comidas

--- lib/api/comidas_controller.dart
+++ lib/api/comidas_controller.dart
@@ -1,19 +1,21 @@
-
 import 'dart:convert';
+import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'package:lajamasana/modelos/comidas_model.dart';
 
 class ComidasController {
 
 	static String _url = "http://10.0.3.2:8000/productos/allcomidas";
+//	static String _url = "http://10.0.3.2:8000/productos/allcomidas?format=json";
+ // static String _url = "http://10.0.3.2:3000/comidas";
 
 	static Future<List<Comida>> getComidas() async{
+
 		final response = await http.get(_url);
-		if(response.statusCode != 200){
+		if(response.statusCode != HttpStatus.ok){
 			return [];
 		}
 		var datos = json.decode(response.body) as List;
-		return [Comida.fromJson(datos[0])];//datos.map<Comida>((json) => Comida.fromJson(json)).toList();
+		return datos.map<Comida>((json) => Comida.fromJson(json)).toList();	
 	}
-
 }
