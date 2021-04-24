817afcbc14c734305aaae9a1cea2d698c49cd328
Miguel Rivadeneira <migeriva@espol.edu.ec>
2021-04-23 20:42:54 -0500

    Presentar la pantalla trabajada

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:lajamasana/pantallas/pantallaLogin.dart';
+import 'package:lajamasana/pantallas/pantallaInfoDia.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,11 +7,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-	debugShowCheckedModeBanner: false,
+        debugShowCheckedModeBanner: false,
         title: 'LaJamaSana',
         theme: ThemeData(
           primaryColor: Colors.white,
         ),
-        home: LoginPage());
+        home: PantallaInformacionDia());
   }
 }
